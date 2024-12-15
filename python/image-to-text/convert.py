a3998e3e16f1d68af4b1d4eca218f902ec3cbb82
LucientZ <koblenzpoisiden@gmail.com>
2024-12-14 19:32:41 -0600

    Added text ascii converter

---
+++ python/image-to-text/convert.py
@@ -0,0 +1,58 @@
+from PIL import Image
+import math
+
+ascii_charset = [' ', '.', ':', '-', '=', '+', 'v', '*', 'W', '#', '%', '@']
+
+def calculate_luminosity(r: int, g: int, b: int) -> int:
+    return 0.299 * r + 0.587 * g + 0.114 * b
+
+def rgb_to_ascii(r: int, g: int, b: int) -> str:
+    index = math.floor(calculate_luminosity(r, g, b) * (len(ascii_charset) - 1) / 255)
+    return ascii_charset[index] 
+
+def rgb_to_ansi(r: int, g: int, b: int) -> int:
+    if r == g and g == b:
+        if r < 8:
+            return 16
+        elif r > 248:
+            return 231
+
+        return round(((r - 8) / 247) * 24) + 232
+
+    return (
+        16 + (36 * round(r / 255 * 5)) + (6 * round(g / 255 * 5)) + round(b / 255 * 5)
+    )
+
+
+def convert_to_text(buffer: list[any], width: int, height: int, use_colors: bool = False) -> str:
+    final_string = ""
+    for i, (r, g, b, a) in enumerate(buffer):
+        if a == 0:
+            char = " "
+        else:
+            char = rgb_to_ascii(r, g, b)
+        final_string += char
+        if i % width == width - 1:
+            final_string += '\n'
+    return final_string
+
+
+def main():
+    in_file = input("Enter Input File: ")
+    out_file = input("Enter Output File: ")
+
+    image = Image.open(in_file).convert("RGBA")
+    width, height = image.size
+    buffer: list[tuple[int, int, int]] = list(image.getdata())
+
+
+    final = convert_to_text(buffer, width, height, False)
+
+    if out_file != "":
+        print(final, file=open("out_file", "w"))
+    else:
+        print(final)
+
+
+if __name__ == "__main__":
+    main()
